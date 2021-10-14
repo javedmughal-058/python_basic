96c51457f70e90b82c260f193c577d8f11742f1f
Muhammad_Javed <91019922+javedmughal-058@users.noreply.github.com>
2021-10-14 23:01:49 +0500

    Print hello world in python.
    This code show you how to display text on screen in python programming.

---
+++ Hello_World.py
@@ -0,0 +1,10 @@
+
+#Syntax to display Hello World.
+
+print("Hello World")
+
+
+#Notes   '#' simple is used to comment. 
+
+    #In python programmming you don't need to include libraries like C/C++. 
+#Code by Muhammad Javed
