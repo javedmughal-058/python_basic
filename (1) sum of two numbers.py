d37f33a3963c305599f9f70d7b535215552d1e1a
Muhammad_Javed <91019922+javedmughal-058@users.noreply.github.com>
2021-10-14 23:08:36 +0500

    Add two numbers.
    by this code, you learn how to declare variables in python.

---
+++ (1) sum of two numbers.py
@@ -0,0 +1,15 @@
+
+#Syntax to display Sum the given numbers.
+#This code will show you hoow to declare variables. 
+#As you see that in python there is no need to mentioned datatype for any type of data.
+
+value1=10
+value2=12
+sum=10+12
+print("Sum =",sum)
+
+
+#Notes   '#' simple is used to comment. 
+
+    #In python programmming you don't need to include libraries like C/C++. 
+#Code by Muhammad Javed
