fc258c99f44e1934871807a4957eacb63a302d5a
Muhammad_Javed <91019922+javedmughal-058@users.noreply.github.com>
2021-10-15 18:14:40 +0500

    How to take values from users in python.
    Program for calculating percentage of marks in Examination.

---
+++ (4)Take Input from user.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 15 18:03:45 2021
+
+@author: Muhammad Javed
+"""
+
+# if else statement in Python
+# Take values from users.
+
+tm=input("Enter Total marks: ")
+om=input("Enter Obtained marks: ")
+per=(om*100)/tm
+if per>=33:
+    print("Congratulation..! You have passed the Exam.")
+else:
+    print("Oops..! You are failed in Exam.")
+    
+    
+#keep in mind the proper indenting. 
+    
