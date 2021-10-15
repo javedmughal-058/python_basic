ef93e4950ccb92bd3fe81336dd655086dd9ef022
Muhammad_Javed <91019922+javedmughal-058@users.noreply.github.com>
2021-10-15 06:17:13 -0700

    Update (4)Take Input from user.py

--- (4)Take Input from user.py
+++ (4)Take Input from user.py
@@ -8,8 +8,8 @@ Created on Fri Oct 15 18:03:45 2021
 # if else statement in Python
 # Take values from users.
 
-tm=input("Enter Total marks: ")
-om=input("Enter Obtained marks: ")
+tm=int(input("Enter Total marks: "))
+om=int(input("Enter Obtained marks: "))
 per=(om*100)/tm
 if per>=33:
     print("Congratulation..! You have passed the Exam.")
