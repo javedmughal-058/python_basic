ad1f65e944577c59ffe6cc5e30d986c2518c6779
Muhammad_Javed <91019922+javedmughal-058@users.noreply.github.com>
2021-10-15 18:08:54 +0500

    If else in Python Language
    by this code you'll learn that 'What is the syntax of if else statement and what is indenting in python.'

---
+++ (3) if else in python.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 15 18:03:45 2021
+
+@author: Muhammad Javed
+"""
+
+# if else statement in Python
+
+a=10
+b=12
+if a>b:
+    print("A is greater")
+else:
+    print("b is greater")
+    
+    
+#keep in mind the proper indenting. 
+    
